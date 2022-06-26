1ac1a702a3ba22f2bd3af8a148ea562bd5c5bf62
Maryam-Inam <maryaminam00@gmail.com>
2022-06-26 23:03:49 +0500

    updated

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -36,9 +36,6 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
       mode: "payment",
       success_url: `${BASE_URL}/payment/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: BASE_URL,
-      shipping_address_collection: {
-        allowed_countries: ["US", "PK"],
-      },
       customer_creation: "if_required",
       // shipping_options: [
       //   {
