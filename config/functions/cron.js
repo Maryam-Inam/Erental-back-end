f02be7fccad5468b171f6a28066156f7790149a3
Maryam-Inam <maryaminam00@gmail.com>
2022-07-06 22:09:35 +0500

    Update cron.js

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -1,27 +1,25 @@
 module.exports = {
-  //   "0 0 12 ? 1/1 MON#1 *": async ({ strapi }) => {
-  "* * * * *": async () => {
-    console.log("checking...");
-    // const bidItems = await strapi.db
-    //   .query("api::bidding-item.bidding-item")
-    //   .findMany({
-    //     populate: "*",
-    //     where: {
-    //       highest_bidder: {
-    //         id: {
-    //           $notNull: true,
-    //         },
-    //       },
-    //     },
-    //   });
+  "0 0 12 ? 1/1 MON#1 *": async () => {
+    const bidItems = await strapi.db
+      .query("api::bidding-item.bidding-item")
+      .findMany({
+        populate: "*",
+        where: {
+          highest_bidder: {
+            id: {
+              $notNull: true,
+            },
+          },
+        },
+      });
 
-    // bidItems.map(async (item) => {
-    //   console.log("bidder email: ", item.highest_bidder.email);
-    //   await strapi.service("api::bidding-order.bidding-order").sendMail({
-    //     data: {
-    //       to: item.highest_bidder.email,
-    //     },
-    //   });
-    // });
+    bidItems.map(async (item) => {
+      console.log("bidder email: ", item.highest_bidder.email);
+      await strapi.service("api::bidding-order.bidding-order").sendMail({
+        data: {
+          to: item.highest_bidder.email,
+        },
+      });
+    });
   },
 };
